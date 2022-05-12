deb9a02599b2b1e6ad84975810916cc306f80d49
Zelalem Mekonnen <mekonnen.zelalem251@gmail.com>
2022-05-12 13:41:55 +0300

    Time: 90 ms (42.02%), Space: 44.9 MB (73.96%) - LeetHub

---
+++ 704-binary-search/704-binary-search.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+
+function search(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        const middle = Math.floor( ((left + right) / 2) );
+        const current = nums[middle];
+        
+        if (current > target) {
+            // ignore the right part from the array and the current index
+            right = middle - 1;
+        } else if (current < target) {
+            // ignore the left part from the array and the current index
+            left = middle + 1
+        } else {
+            return middle;
+        }
+    }
+    // return -1 if middle did never equal the target
+    return -1;
+};
