95028d336f5cdd59ef3308c17431b8f95584c611
Zelalem Mekonnen <mekonnen.zelalem251@gmail.com>
2022-05-28 12:16:33 +0300

    Time: 3589 ms (7.29%), Space: 15.2 MB (34.52%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,14 +1,5 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        min = nums[0];
-        max = nums[0];
-        
-        for i in nums:
-            if(min>i):
-                min = i
-            if(max<i):
-                max = i
-                
+    def missingNumber(self, nums: List[int]) -> int:    
         for i in range(0,len(nums)+1):
             if(i not in nums):
                 return i
