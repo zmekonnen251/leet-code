6f14a09959b47c451944960ce2560d37217367fe
Zelalem Mekonnen <mekonnen.zelalem251@gmail.com>
2022-06-27 00:17:31 +0100

    Time: 87 ms (73.63%), Space: 48.1 MB (38.59%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} cardPoints
+ * @param {number} k
+ * @return {number}
+ */
+var maxScore = function(cardPoints, k) {
+    let left = 0; 
+    let right = 0; 
+    
+    for(let i = 0; i < k; i++){
+        left = left + cardPoints[i];
+    }
+    let maxScore = left
+    for(let i = k - 1; i >= 0; i--){
+        left = left - cardPoints[i]
+        right = right + cardPoints[cardPoints.length-(k-i)]
+        maxScore = Math.max(maxScore, left + right)
+    }
+    return maxScore
+};
