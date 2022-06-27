58f549329abae362149d66dc0cdd71d390af0c75
Zelalem Mekonnen <mekonnen.zelalem251@gmail.com>
2022-06-27 23:42:17 +0100

    Time: 145 ms (18.45%), Space: 50.3 MB (54.51%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -4,11 +4,11 @@
  */
 var maxSubArray = function(nums) {
     let max_so_far = nums[0];
-    let max_end_here = nums[0];
+    let max_i = nums[0];
     
     for(let i=1;i<nums.length;i++){
-        max_end_here = Math.max( nums[i] , max_end_here + nums[i]);
-        max_so_far = Math.max(max_so_far,max_end_here);
+        max_i = Math.max( nums[i] , max_i + nums[i]);
+        max_so_far = Math.max(max_so_far,max_i);
     }
     
     return max_so_far;
