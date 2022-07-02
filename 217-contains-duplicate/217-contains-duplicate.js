3898f6187a9e256d19e8ed2063f7ed7a43cb980a
Zelalem Mekonnen <mekonnen.zelalem251@gmail.com>
2022-07-02 23:50:22 +0100

    Time: 159 ms (34.82%), Space: 51.6 MB (22.99%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,23 +3,25 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-//     hashTable = {}
+    const hashTable = {}
     
-//     for(let i=0;i<nums.length;i++){
-//         if(hashTable[nums[i]]===undefined) {
-//             hashTable[nums[i]]=i;
-//         }else if(i!==nums.length){
-//             return true
-//         }
-        
-//     }
-//     return false;
     for(let i=0;i<nums.length;i++){
-        for(let j=0;j<nums.length && i!==j;j++){
-            if(nums[i]===nums[j]){
-                return true;
-            }
+        if(hashTable[nums[i]]===undefined) {
+            hashTable[nums[i]]=i;
+        }else if(i!==nums.length){
+            return true
         }
+        
     }
     return false;
+    
+    // for(let i=0;i<nums.length;i++){
+    //     for(let j=0;j<nums.length && i!==j;j++){
+    //         if(nums[i]===nums[j]){
+    //             return true;
+    //         }
+    //     }
+    // }
+    // return false;
+    
 };
